8dc712e55cb386d60a9e0a2ccbc890f38ed51ad4
binodlamiochhane31 <lamibinod31@gmail.com>
2025-05-10 21:35:52 +0545

    splash screen created

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hisab_kitab/view/login_view.dart';
+import 'package:hisab_kitab/view/splash_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LoginView(), debugShowCheckedModeBanner: false);
+    return MaterialApp(home: SplashScreen(), debugShowCheckedModeBanner: false);
   }
 }
---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:hisab_kitab/view/login_view.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (BuildContext context) => LoginView()),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffe5d3),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('lib/assets/image.png', height: 100),
+            SizedBox(height: 50),
+            CircularProgressIndicator(
+              color: Colors.orange,
+              // you can set color
+            ),
+            SizedBox(height: 50),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                children: [
+                  TextSpan(
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    text: "Simplify Your Shop's Hisab",
+                  ),
+                  TextSpan(
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.orange,
+                    ),
+                    text: "Kitab",
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
