cb8cb8206c08550d77ef0467cba6efe6bc69f76b
binodlamiochhane31 <lamibinod31@gmail.com>
2025-06-17 23:18:38 +0545

    Added local repository for login and register

---
+++ lib/features/auth/data/repository/local_repository/user_local_repository.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hisab_kitab/core/error/failure.dart';
+import 'package:hisab_kitab/features/auth/data/data_source/local_data_source/user_local_data_source.dart';
+import 'package:hisab_kitab/features/auth/domain/entity/user_entity.dart';
+import 'package:hisab_kitab/features/auth/domain/repository/user_repository.dart';
+
+class UserLocalRepository implements IUserRepository {
+  final UserLocalDataSource _userLocalDataSource;
+
+  UserLocalRepository({required UserLocalDataSource userLocalDataSource})
+    : _userLocalDataSource = userLocalDataSource;
+  @override
+  Future<Either<Failure, String>> loginUser(
+    String email,
+    String password,
+  ) async {
+    try {
+      final result = await _userLocalDataSource.loginUser(email, password);
+      return Right(result);
+    } catch (e) {
+      return Left(LocalDatabaseFailure(message: "Login Failed: $e"));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> registerUser(UserEntity user) async {
+    try {
+      await _userLocalDataSource.registerUser(user);
+      return Right(null);
+    } catch (e) {
+      return Left(LocalDatabaseFailure(message: "Registration Failed: $e"));
+    }
+  }
+}
--- lib/features/auth/domain/repository/user_repository.dart
+++ lib/features/auth/domain/repository/user_repository.dart
@@ -1,7 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:hisab_kitab/core/error/failure.dart';
+import 'package:hisab_kitab/features/auth/domain/entity/user_entity.dart';
 
 abstract interface class IUserRepository {
-  Future<Either<Failure, void>> registerUser();
+  Future<Either<Failure, void>> registerUser(UserEntity user);
   Future<Either<Failure, String>> loginUser(String email, String password);
 }
