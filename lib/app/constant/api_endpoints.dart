1560abae82cdf8ea5371725b937e378ecb469f80
binodlamiochhane31 <lamibinod31@gmail.com>
2025-06-29 22:13:59 +0545

    api endpoints added for login and register

---
+++ lib/app/constant/api_endpoints.dart
@@ -0,0 +1,15 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  static const connectionTimeout = Duration(seconds: 1000);
+  static const receiveTimeout = Duration(seconds: 1000);
+
+  static const String serverAddress = "http://localhost:6060";
+
+  static const String baseUrl = "$serverAddress/api";
+  static const String imageUrl = "$serverAddress/uploads";
+
+  //Auth
+  static const String register = "/auth/register";
+  static const String login = "/auth/login";
+}
