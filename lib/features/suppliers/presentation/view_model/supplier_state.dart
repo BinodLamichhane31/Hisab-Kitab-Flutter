0989c20517ee962077a6db9227e42a41420ec312
binodlamiochhane31 <lamibinod31@gmail.com>
2025-07-22 15:24:11 +0545

    Implemented search feature in customer and supplier pages

--- lib/features/suppliers/presentation/view_model/supplier_state.dart
+++ lib/features/suppliers/presentation/view_model/supplier_state.dart
@@ -5,30 +5,35 @@ class SupplierState extends Equatable {
   final List<SupplierEntity> suppliers;
   final bool isLoading;
   final String? errorMessage;
+  final String? search;
 
   const SupplierState({
     required this.suppliers,
     required this.isLoading,
     this.errorMessage,
+    this.search,
   });
 
   const SupplierState.initial()
     : suppliers = const [],
       isLoading = false,
-      errorMessage = null;
+      errorMessage = null,
+      search = null;
 
   SupplierState copyWith({
     List<SupplierEntity>? suppliers,
     bool? isLoading,
     String? errorMessage,
+    String? search,
   }) {
     return SupplierState(
       suppliers: suppliers ?? this.suppliers,
       isLoading: isLoading ?? this.isLoading,
       errorMessage: errorMessage,
+      search: search ?? this.search,
     );
   }
 
   @override
-  List<Object?> get props => [suppliers, isLoading, errorMessage];
+  List<Object?> get props => [suppliers, isLoading, errorMessage, search];
 }
